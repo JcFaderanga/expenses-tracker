2a19a416a9946117e614462a572a978519196072
Jeysi/ <119491303+JcFaderanga@users.noreply.github.com>
2024-04-15 09:04:33 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const add_box = document.querySelector(".add-container");
 const add_button = document.querySelector(".bx-plus");
 const add_exit = document.querySelector(".bx-x");
+const item = document.querySelector(".item");
 
 
-add_button.addEventListener('click',function(){
+add_button.onclick = function(){
     add_box.style.display = "flex";
     document.querySelector("nav").style.background = "#0e6dbd";
-});
+};
 
-add_exit.addEventListener('click',function(){
+add_exit.onclick = function(){
     add_box.style.display = "none";
     document.querySelector("nav").style.background = "white";
-});
+};
 
+item.onclick = function(){
+item.style.height = "200px";
+};
 
 
